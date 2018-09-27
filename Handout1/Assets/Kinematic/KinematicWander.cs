a199c280a82b386ac20833fde13a2e7a0d69a0c8
manelmourelo <manelmourelo.1@gmail.com>
2018-09-27 19:32:23 +0200

    Homework

--- Handout1/Assets/Kinematic/KinematicWander.cs
+++ Handout1/Assets/Kinematic/KinematicWander.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class KinematicWander : MonoBehaviour {
 
 	public float max_angle = 0.5f;
+    private float timer = 0.0f;
+    private Vector3 vel = Vector3.zero;
 
 	Move move;
 
@@ -22,8 +24,23 @@ public class KinematicWander : MonoBehaviour {
 	void Update () 
 	{
         // TODO 9: Generate a velocity vector in a random rotation (use RandomBinominal) and some attenuation factor
-        float angle = 0.8f * RandomBinominal();
-        Vector3 vel = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle));
+        if (timer <= 0.0f)
+        {
+            setNewDirection();
+            timer = 5.0f;
+        }
         move.SetMovementVelocity(vel);
+        float angle = Mathf.Atan2(move.mov_velocity.x, move.mov_velocity.z);
+        angle *= 57.2958f;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
+        timer -= Time.deltaTime;
+    }
+
+    void setNewDirection()
+    {
+        float angle = 0.8f * RandomBinominal();
+        angle *= 57.2958f;
+        vel = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle));
+        vel.Normalize();
     }
 }
