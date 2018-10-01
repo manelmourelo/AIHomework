30ea1bf6eca981288397777c3028cc17ed106f5d
manelmourelo <manelmourelo.1@gmail.com>
2018-10-01 09:52:16 +0200

    Todo5

--- Handout2/Assets/Steering/SteeringPursue.cs
+++ Handout2/Assets/Steering/SteeringPursue.cs
@@ -22,8 +22,26 @@ public class SteeringPursue : MonoBehaviour {
 
 	public void Steer(Vector3 target, Vector3 velocity)
 	{
-		// TODO 6: Create a fake position to represent
-		// enemies predicted movement. Then call Steer()
-		// on our Steering Arrive
+        // TODO 6: Create a fake position to represent
+        // enemies predicted movement. Then call Steer()
+        // on our Steering Arrive
+        Vector3 direction = target - transform.position;
+        float distance = direction.magnitude;
+
+        float speed = velocity.magnitude;
+
+        float prediction = 0.0f;
+        if (speed <= (distance/max_prediction))
+        {
+            prediction = max_prediction;
+        }
+        else
+        {
+            prediction = distance / speed;
+        }
+
+        target += move.movement * prediction; 
+
+        arrive.Steer(target);
 	}
 }
