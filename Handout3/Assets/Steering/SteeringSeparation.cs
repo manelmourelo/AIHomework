fecbeb00fb1fd07404f01f596f91ca88394dc88f
manelmourelo <manelmourelo.1@gmail.com>
2018-10-01 16:53:31 +0200

    Todo1

--- Handout3/Assets/Steering/SteeringSeparation.cs
+++ Handout3/Assets/Steering/SteeringSeparation.cs
@@ -21,7 +21,21 @@ public class SteeringSeparation : MonoBehaviour {
 		// 1- Find other agents in the vicinity (use a layer for all agents)
 		// 2- For each of them calculate a escape vector using the AnimationCurve
 		// 3- Sum up all vectors and trim down to maximum acceleration
-	}
+        Collider[] tanks = Physics.OverlapSphere(transform.position, search_radius, mask.value);
+        
+        foreach (Collider col in tanks)
+        {
+            if (transform.GetComponent<Collider>() != col)
+            {
+                
+                Vector3 repulsion = transform.position - col.transform.position;
+                repulsion.Normalize();
+                repulsion *= move.max_mov_acceleration;
+                repulsion *= falloff.Evaluate(repulsion.magnitude);
+                move.AccelerateMovement(repulsion);
+            }
+        }
+    }
 
 	void OnDrawGizmosSelected() 
 	{
