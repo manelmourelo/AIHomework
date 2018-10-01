5e3d919313b07e82cac3b21611a193f3a03440d7
manelmourelo <manelmourelo.1@gmail.com>
2018-10-01 09:13:10 +0200

    TODO 4 correct rotation

--- Handout2/Assets/Steering/SteeringAlign.cs
+++ Handout2/Assets/Steering/SteeringAlign.cs
@@ -20,10 +20,7 @@ public class SteeringAlign : MonoBehaviour {
         // TODO 4: As with arrive, we first construct our ideal rotation
         // then accelerate to it. Use Mathf.DeltaAngle() to wrap around PI
         // Is the same as arrive but with angular velocities
-        float rotArrow = Mathf.Atan2(move.arrow.transform.forward.x, move.arrow.transform.forward.z);
-        Vector3 axis = new Vector3(0,1,0);
-        move.target.transform.Rotate(axis, rotArrow);
-        float rotTar = Mathf.Atan2(move.target.transform.forward.x, move.target.transform.forward.z);
+        float rotTar = Mathf.Atan2(move.movement.x, move.movement.z);
         float rot = Mathf.Atan2(transform.forward.x, transform.forward.z);
         rotTar *= Mathf.Rad2Deg;
         rot *= Mathf.Rad2Deg;
@@ -52,6 +49,8 @@ public class SteeringAlign : MonoBehaviour {
             acceleration /= absAcceleration;
             acceleration *= move.max_rot_acceleration;
         }
+
+        rotDif = Mathf.Clamp(acceleration, -move.max_rot_acceleration, move.max_rot_acceleration);
 
         if (rotSize < min_angle)
         {
