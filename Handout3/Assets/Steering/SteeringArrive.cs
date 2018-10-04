37fd1b03137d4f545cf39e5091597c582b1d376a
manelmourelo <manelmourelo.1@gmail.com>
2018-10-04 18:37:59 +0200

    Todo2

--- Handout3/Assets/Steering/SteeringArrive.cs
+++ Handout3/Assets/Steering/SteeringArrive.cs
@@ -3,57 +3,55 @@ using System.Collections;
 
 public class SteeringArrive : MonoBehaviour {
 
-	public float min_distance = 0.1f;
-	public float slow_distance = 5.0f;
-	public float time_to_target = 0.1f;
+    public float min_distance = 0.1f;
+    public float slow_distance = 5.0f;
+    public float time_to_target = 0.1f;
 
-	Move move;
+    Move move;
 
-	// Use this for initialization
-	void Start () { 
-		move = GetComponent<Move>();
-	}
+    // Use this for initialization
+    void Start()
+    {
+        move = GetComponent<Move>();
+    }
 
-	// Update is called once per frame
-	void Update () 
-	{
-		Steer(move.target.transform.position);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        Steer(move.target.transform.position);
+    }
 
-	public void Steer(Vector3 target)
-	{
-		if(!move)
-			move = GetComponent<Move>();
+    public void Steer(Vector3 target)
+    {
+        if (!move)
+            move = GetComponent<Move>();
 
-		// Velocity we are trying to match
-		float ideal_velocity = 0.0f;
-		Vector3 diff = target - transform.position;
-
-		if(diff.magnitude < min_distance)
-			move.SetMovementVelocity(Vector3.zero);
-
-		// Decide wich would be our ideal velocity
-		if(diff.magnitude > slow_distance)
-			ideal_velocity = move.max_mov_velocity;
-		else
-			ideal_velocity = move.max_mov_velocity * diff.magnitude / slow_distance;
-
-		// Create a vector that describes the ideal velocity
-		Vector3 ideal_movement = diff.normalized * ideal_velocity;
-
-		// Calculate acceleration needed to match that velocity
-		Vector3 acceleration = ideal_movement - move.movement;
-		acceleration /= time_to_target;
-
-		// Cap acceleration
-		if(acceleration.magnitude > move.max_mov_acceleration)
-		{
-			acceleration.Normalize();
-			acceleration *= move.max_mov_acceleration;
-		}
-
-		move.AccelerateMovement(acceleration);
-	}
+        // TODO 3: Create a vector to calculate our ideal velocity
+        // then calculate the acceleration needed to match that velocity
+        // before sending it to move.AccelerateMovement() clamp it to 
+        // move.max_mov_acceleration
+        Vector3 dis = move.target.transform.position - transform.position;
+        Vector3 acceleration = dis * move.max_mov_acceleration;
+        float disMag = dis.magnitude;
+        dis.Normalize();
+        if (disMag <= slow_distance)
+        {
+            dis = time_to_target * dis * disMag;
+            //if (disMag < min_distance)
+            //{
+            //    move.AccelerateMovement(Vector3.zero);
+            //}
+            acceleration = (dis - move.movement) * move.max_mov_acceleration / disMag;
+        }
+        if (disMag < min_distance)
+        {
+            move.AccelerateMovement(Vector3.zero);
+        }
+        else
+        {
+            move.AccelerateMovement(acceleration);
+        }
+    }
 
 	void OnDrawGizmosSelected() 
 	{
--- Handout3/Assets/Steering/SteeringObstacleAvoidance.cs
+++ Handout3/Assets/Steering/SteeringObstacleAvoidance.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
+class Test : System.Object
+{
+    public RaycastHit[] hits;
+}
+
 public class SteeringObstacleAvoidance : MonoBehaviour {
 
 	public LayerMask mask;
 	public float avoid_distance = 5.0f;
+    private RaycastHit hit1;
 
-
-	Move move;
+    Move move;
 	SteeringSeek seek;
 
 	// Use this for initialization
@@ -19,12 +25,25 @@ public class SteeringObstacleAvoidance : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		// TODO 2: Agents must avoid any collider in their way
-		// 1- Create your own (serializable) class for rays and make a public array with it
-		// 2- Calculate a quaternion with rotation based on movement vector
-		// 3- Cast all rays. If one hit, get away from that surface using the hitpoint and normal info
-		// 4- Make sure there is debug draw for all rays (below in OnDrawGizmosSelected)
-	}
+        // TODO 2: Agents must avoid any collider in their way
+        // 1- Create your own (serializable) class for rays and make a public array with it
+        // 2- Calculate a quaternion with rotation based on movement vector
+        // 3- Cast all rays. If one hit, get away from that surface using the hitpoint and normal info
+        // 4- Make sure there is debug draw for all rays (below in OnDrawGizmosSelected)
+
+        Quaternion rotation = Quaternion.LookRotation(move.movement);
+
+        if(Physics.Raycast(transform.position, move.movement, out hit1, mask))
+        {
+            if (hit1.distance <= avoid_distance)
+            {
+                Vector3 acceleration = hit1.point - hit1.normal;
+                move.AccelerateMovement(acceleration);
+            }
+        }
+
+
+    }
 
 	void OnDrawGizmosSelected() 
 	{
@@ -34,8 +53,8 @@ public class SteeringObstacleAvoidance : MonoBehaviour {
 			float angle = Mathf.Atan2(move.movement.x, move.movement.z);
 			Quaternion q = Quaternion.AngleAxis(Mathf.Rad2Deg * angle, Vector3.up);
 
-			// TODO 2: Debug draw thoise rays (Look at Gizmos.DrawLine)
-
-		}
+            // TODO 2: Debug draw thoise rays (Look at Gizmos.DrawLine)
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit1.distance, Color.white);
+        }
 	}
 }
