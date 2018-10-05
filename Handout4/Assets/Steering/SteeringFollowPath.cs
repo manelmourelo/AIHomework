148afe0da1fc6fd13db20b359c9d92b607d1821c
manelmourelo <manelmourelo.1@gmail.com>
2018-10-05 11:53:23 +0200

    Follow the path

--- Handout4/Assets/Steering/SteeringFollowPath.cs
+++ Handout4/Assets/Steering/SteeringFollowPath.cs
@@ -12,7 +12,7 @@ public class SteeringFollowPath : MonoBehaviour {
     Vector3 point = Vector3.zero;
     public GameObject path;
     private uint currentPoint = 0;
-    public float distanceToSearch = 5.0f;
+    public float distanceToSearch = 1.0f;
 
     // Use this for initialization
     void Start () {
@@ -45,7 +45,7 @@ public class SteeringFollowPath : MonoBehaviour {
         // TODO 2: Check if the tank is close enough to the desired point
         // If so, create a new point further ahead in the path
 
-        Vector3 distance = math.CalcPositionByClosestPoint(point);
+        Vector3 distance = transform.position - point;
         if (distance.magnitude <= distanceToSearch)
         {
             currentPoint++;
