1ce6610a5b16f03227ad1f71151cd1d569ab7165
manelmourelo <manelmourelo.1@gmail.com>
2018-10-15 16:47:02 +0200

    Add navigation

---
+++ Handout6/Assets/SetNavDestination.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SetNavDestination : MonoBehaviour {
+
+    public NavMeshAgent tank;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        tank.destination = transform.position;
+        
+	}
+}
