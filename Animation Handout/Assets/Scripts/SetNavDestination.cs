ade995631097ebe68720f214a0d70af3f9b145cb
manelmourelo <manelmourelo.1@gmail.com>
2018-10-19 11:39:50 +0200

    Animation handout finished

---
+++ Animation Handout/Assets/Scripts/SetNavDestination.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SetNavDestination : MonoBehaviour {
+
+    public NavMeshAgent character;
+    public Animator animations;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        character.destination = transform.position;
+
+
+        if (Vector3.Distance(character.transform.position, transform.position) > 0.0f)
+        {
+            animations.SetBool("Movement", true);
+        }
+        //else
+        //{
+        //    animations.SetBool("Movement", false);
+        //}
+
+        Vector3 movement = character.transform.position - transform.position;
+        movement.Normalize();
+
+        animations.SetFloat("vel x", movement.x);
+
+        animations.SetFloat("vel y", movement.z);
+
+    }
+}
